41db00d6ff51162a8347c403115eded777e5e4dd
Jacob Bevilacqua <jacobbev@gmail.com>
2016-05-08 07:58:37 -0700

    Update to Spot page

--- app/controllers/sound_levels_controller.rb
+++ app/controllers/sound_levels_controller.rb
@@ -4,7 +4,7 @@ class SoundLevelsController < ApplicationController
   	@spot = Spot.find(params[:spot_id])
   	@sound_level = SoundLevel.create(:spot_id => @spot.id.to_i, :user_id => user.id.to_i, :value => params[:value])
   	if @sound_level.valid?
-  		@spot.current_sound_level = ((@sound_level.to_i + @spot.current_sound_level.to_i)) / 2
+  		@spot.current_sound_level = ((@sound_level.value.to_i + @spot.current_sound_level.to_i)) / 2
   		@spot.save
   	end
   	redirect_to @spot
